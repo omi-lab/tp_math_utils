1b5a801804c4fee23fe59b0001daefa93152fe5e
Tom Paynter <tompaynter@tdpe.co.uk>
2018-10-19 15:50:12 +0100

    Merged from private repos.

--- inc/tp_math_utils/Globals.h
+++ inc/tp_math_utils/Globals.h
@@ -1,13 +1,13 @@
 #ifndef tp_math_utils_Globals_h
 #define tp_math_utils_Globals_h
 
-#if defined(TP_MATH_UTILS_LIBRARY)
-#  define TP_MATH_UTILS_SHARED_EXPORT
-#else
-#  define TP_MATH_UTILS_SHARED_EXPORT
-#endif
+#include "tp_utils/StringID.h"
 
-#include "tp_utils/Globals.h"
+#if defined(TP_MATH_UTILS_LIBRARY)
+#  define TP_MATH_UTILS_SHARED_EXPORT TP_EXPORT
+#else
+#  define TP_MATH_UTILS_SHARED_EXPORT TP_IMPORT
+#endif
 
 #include "glm/glm.hpp"
 
