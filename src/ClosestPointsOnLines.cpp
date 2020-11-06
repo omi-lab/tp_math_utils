5a774408e3f6078c2f9d07c3cb35bc7674d374eb
Tom Paynter <tompaynter@tdpe.co.uk>
2020-11-06 15:10:05 +0000

    Generate new object instance IDs when populating a scene.

--- src/ClosestPointsOnLines.cpp
+++ src/ClosestPointsOnLines.cpp
@@ -30,7 +30,7 @@ void closestPointsOnLines(const DRay& S1, const DRay& S2, glm::dvec3& P1, glm::d
   double     tc, tN, tD = D;       // tc = tN / tD, default tD = D >= 0
 
   // compute the line parameters of the two closest points
-  if (D < SMALL_NUM) { // the lines are almost parallel
+  if(D < SMALL_NUM) { // the lines are almost parallel
     sN = 0.0;         // force using point near on segment S1
     sD = 1.0;         // to prevent possible division by 0.0 later
     tN = e;
@@ -39,36 +39,36 @@ void closestPointsOnLines(const DRay& S1, const DRay& S2, glm::dvec3& P1, glm::d
   else {                 // get the closest points on the infinite lines
     sN = (b*e - c*d);
     tN = (a*e - b*d);
-    if (sN < 0.0) {        // sc < 0 => the s=0 edge is visible
+    if(sN < 0.0) {        // sc < 0 => the s=0 edge is visible
       sN = 0.0;
       tN = e;
       tD = c;
     }
-    else if (sN > sD) {  // sc > 1  => the s=1 edge is visible
+    else if(sN > sD) {  // sc > 1  => the s=1 edge is visible
       sN = sD;
       tN = e + b;
       tD = c;
     }
   }
 
-  if (tN < 0.0) {            // tc < 0 => the t=0 edge is visible
+  if(tN < 0.0) {            // tc < 0 => the t=0 edge is visible
     tN = 0.0;
     // recompute sc for this edge
-    if (-d < 0.0)
+    if(-d < 0.0)
       sN = 0.0;
-    else if (-d > a)
+    else if(-d > a)
       sN = sD;
     else {
       sN = -d;
       sD = a;
     }
   }
-  else if (tN > tD) {      // tc > 1  => the t=1 edge is visible
+  else if(tN > tD) {      // tc > 1  => the t=1 edge is visible
     tN = tD;
     // recompute sc for this edge
-    if ((-d + b) < 0.0)
+    if((-d + b) < 0.0)
       sN = 0;
-    else if ((-d + b) > a)
+    else if((-d + b) > a)
       sN = sD;
     else {
       sN = (-d +  b);
