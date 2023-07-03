d18412b7e9495bd12d7114953f5397ceaf35d6f8
Tom Paynter <tompaynter@tdpe.co.uk>
2023-07-03 16:56:38 +0100

    Refactor of the asset fetcher.

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -24,32 +24,35 @@ bool rayPlaneIntersectionImpl(const Ray& ray, const Plane& plane, glm::vec<3,FLO
   const auto  Rd = ray.p1-ray.p0;
 
   auto denom = glm::dot(Po-Ro,Pn);
-  if(std::fabs(denom) < 1e-10){
+  if(std::fabs(denom) < 1e-10f)
+  {
     intersection  = Po;
     return true;
   }
   auto Pp = Ro + (denom/glm::dot(Pn,Pn))*Pn;
 
   auto nom = glm::dot(Pp-Ro,Pp-Ro);
-  if( std::fabs(denom) < 1e-10){
+  if( std::fabs(denom) < 1e-10f)
+  {
     intersection = Pp;
     return true;
   }
 
   denom = glm::dot(Rd,Pp-Ro);
-  if(std::fabs(denom) < 1e-10)
+  if(std::fabs(denom) < 1e-10f)
     return false;
 
   intersection = Ro + (nom/denom)*Rd;
   return true;
 }
 
-
+//##################################################################################################
 bool rayPlaneIntersection(const Ray& ray, const Plane& plane, glm::vec3& intersection)
 {
   return rayPlaneIntersectionImpl(ray, plane, intersection);
 }
 
+//##################################################################################################
 bool rayPlaneIntersection(const Ray& ray, const Plane& plane, glm::dvec3& intersection)
 {
   return rayPlaneIntersectionImpl(ray, plane, intersection);
